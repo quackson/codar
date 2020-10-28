7b959deccfe07df55ceac7ebb75abe147cb0ce10
quackson <1700012888@pku.edu.cn>
2020-10-28 21:57:14 +0800

    add new page create_activity

--- pages/create_activity/create_activity.js
+++ pages/create_activity/create_activity.js
@@ -5,12 +5,93 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    groupID:1,
+    groupName:"initialgroup",
+    prior_:[
+      0,1,2,3,4
+    ],
+    content:'',
+    title:'',
+    prior_index:-1,
+    startTime: '00:00',
+    startDate: '2020-10-28',
+    endTime: '00:00',
+    endDate: '2020-10-28',
+    users:[
+      {
+        userID:0,
+        checked:false,
+        role:1
+      },
+      {
+        userID:1,
+        checked:false,
+        role:0
+      },
+      {
+        userID:2,
+        checked:false,
+        role:0
+      },
+      {
+        userID:3,
+        checked:false,
+        role:0
+      },
+      {
+        userID:4,
+        checked:false,
+        role:0
+      }
+    ]
+  },
+  PickerChange(e) {
+    console.log(e);
+    this.setData({
+      prior_index: e.detail.value
+    })
   },
 
+  startTimeChange(e) {
+    this.setData({
+      startTime: e.detail.value
+    })
+  },
+  startDateChange(e) {
+    this.setData({
+      startDate: e.detail.value
+    })
+  },
+  endTimeChange(e) {
+    this.setData({
+      endTime: e.detail.value
+    })
+  },
+  endDateChange(e) {
+    this.setData({
+      endDate: e.detail.value
+    })
+  },
+  titleInput(e){
+    this.setData({
+      title: e.detail.value
+    });
+  },
+  contentInput(e){
+    this.setData({
+      content: e.detail.value
+    });
+  },
+  checkboxChange(e){
+    //console.log(e);
+    let temp=this.data.users[e.target.dataset.index]['checked'];
+    this.data.users[e.target.dataset.index]['checked']=!temp;
+    //console.log(this.data.users[e.target.dataset.index]['checked']);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
+  
   onLoad: function (options) {
 
   },
